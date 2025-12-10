f067db543818b5b8dde1d79d5ec3f2b7f247e825
kazuki.okahashi <okarians.302.dev@gmail.com>
2025-12-10 09:34:06 +0900

    ホワイトリストで認証を回避するよう修正

--- app.vue
+++ app.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { Auth } from "@supa-kit/auth-ui-vue";
+import { isPublicPath } from "~/utils/publicRoutes";
 
+const route = useRoute();
 const { $supabase } = useNuxtApp();
 const { isLogin } = useAuth();
+
+const isRoutePublic = computed(() => isPublicPath(route.path));
 </script>
 
 <template>
   <v-app>
     <Auth
-      v-if="!isLogin"
+      v-if="!isLogin && !isRoutePublic"
       :supabase-client="$supabase"
       :providers="['google', 'github']"
       :appearance="{
         theme: ThemeSupa,
       }"
     />
-    <router-view v-else />
+    <NuxtPage v-else />
   </v-app>
 </template>
